0d9bccbfb7a2d8f9832ce14702e90233f6e994a0
suntootoo <suntootoo@gmail.com>
2017-07-25 17:17:18 +0800

    init & todo_api part one (tests and basic)

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  # Association Test
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:todo) }
+  # Validation Test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:name) }
+end
