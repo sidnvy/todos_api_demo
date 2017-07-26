6158dde9ef15a39904602fde6a2df88fac41e396
suntootoo <suntootoo@gmail.com>
2017-07-26 14:35:04 +0800

    authentication & tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,7 +56,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   config.include FactoryGirl::Syntax::Methods
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
